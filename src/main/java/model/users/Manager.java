c10d2a630bc96316ac4c96f6057323f99ebab972
mtndaghyani <matin.daghyani@gmail.com>
2020-04-19 00:59:19 +0430

    Update User and Manager

---
+++ src/main/java/model/users/Manager.java
@@ -0,0 +1,30 @@
+package model.users;
+
+import model.requests.Request;
+
+import java.util.ArrayList;
+
+public class Manager extends User {
+    private static ArrayList<Request> requests;
+
+    public Manager(String username, String password, String firstName,
+                   String lastName, String email, String phoneNo) {
+        super(username, password, firstName, lastName, email, phoneNo);
+    }
+
+    public static ArrayList<Request> getRequests() {
+        return requests;
+    }
+
+    public void addRequest(Request request){}
+
+    public static void loadData(){}
+
+    public static void saveData(){}
+
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
