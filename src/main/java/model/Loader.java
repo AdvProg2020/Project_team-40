6dc9f57733ec8f823803fe401d7ef23613b0acf7
mtndaghyani <matin.daghyani@gmail.com>
2020-04-19 01:22:26 +0430

    Add Loader

---
+++ src/main/java/model/Loader.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Loader {
+    private Loader loader;
+
+    private Loader(){}
+
+    public void loadData(){}
+
+    public void saveData(){}
+
+    public Loader getLoader() {
+        return loader;
+    }
+}
