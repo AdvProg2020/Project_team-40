c6f1aa9863c03e437cf8d0288caa37b175e27b36
soligits <m.soltani.252@gmail.com>
2020-04-19 09:56:00 +0430

    added AllProductsController

---
+++ src/main/java/Controller/Menus/AllProductsController.java
@@ -0,0 +1,77 @@
+package Controller.Menus;
+
+import model.Product;
+import model.properties.Property;
+
+import java.util.ArrayList;
+
+public class AllProductsController{
+
+    private static AllProductsController allProductsController;
+    private ArrayList<Property> currentFilters;
+    private String currentSort;
+    private ArrayList<Product> allProductsProccessed;
+
+    public ArrayList<String> getAllCategories(){
+        return null;
+    }
+
+    public ArrayList<String> getAllSubCategories(String parentName){
+        return null;
+    }
+
+    public ArrayList<String> getAvailableFilters(){
+        return null;
+    }
+
+    public void addValuePropertyToFilter(String type, double minValue, double maxValue){
+
+    }
+
+    public void addStringPropertyToFilter(String type, String value){
+
+    }
+
+    public void disableFilter(String type){
+
+    }
+
+    public ArrayList<Property> getCurrentFilters(){
+        return null;
+    }
+
+    public ArrayList<String> getAvailableSorts(){
+        return null;
+    }
+
+    public void addSort(String type){
+
+    }
+
+    public void disableSort(){
+
+    }
+
+    public String getCurrentSort(){
+        return null;
+    }
+
+    public Product getProduct(String productID){
+        return null;
+    }
+
+    public ArrayList<String> getAllProductNames(){
+        return null;
+    }
+
+    private AllProductsController(){
+
+    }
+
+    public static AllProductsController getInstance(){
+        if(allProductsController == null)
+            allProductsController = new AllProductsController();
+
+        return allProductsController;
+    }
+}
