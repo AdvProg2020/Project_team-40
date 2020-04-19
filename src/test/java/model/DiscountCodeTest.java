8064741ddbca3e8dca12b0cd4a3878bcab508fca
mtndaghyani <matin.daghyani@gmail.com>
2020-04-19 22:31:10 +0430

    Add DiscountCodeTest unit test

---
+++ src/test/java/model/DiscountCodeTest.java
@@ -0,0 +1,19 @@
+package model;
+
+import model.DiscountCode;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DiscountCodeTest {
+    @Test
+    public void testDateValidation() throws ParseException {
+        Date start = new SimpleDateFormat("yyyy-mm-dd").parse("2020-03-25");
+        Date end = new SimpleDateFormat("yyyy-mm-dd").parse("2020-04-25");
+        DiscountCode code = new DiscountCode(start, end, 10, 1000, 2);
+        Assert.assertFalse(code.checkIfExpired());
+    }
+}
