383ed451aa2e742865febb4c3c96d7a76adc56d0
mtndaghyani <matin.daghyani@gmail.com>
2020-04-26 18:50:04 +0430

    Implement createDiscount and removeProduct methods

--- src/main/java/Controller/Accounts/ManagerAccountController.java
+++ src/main/java/Controller/Accounts/ManagerAccountController.java
@@ -3,11 +3,15 @@ package Controller.Accounts;
 import exceptions.AccountsException;
 import model.Category;
 import model.DiscountCode;
+import model.Product;
 import model.requests.Request;
+import model.users.Customer;
 import model.users.Manager;
 import model.users.User;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 public class ManagerAccountController extends AccountController{
@@ -39,13 +43,38 @@ public class ManagerAccountController extends AccountController{
         User.deleteUser(user);
     }
 
-    public void removeProduct(String productID){
-
+    public void removeProduct(String productID) throws AccountsException {
+        Product product = Product.getProductById(productID);
+        if(product == null)
+            throw new AccountsException("Product not found.");
+        Product.removeProduct(productID);
     }
 
-    public void createDisount(String code, String startDate, String endDate, double percentage, double maxDiscount, int countForEachUser,
-                              ArrayList<String> listOfUsernames){
-
+    private void addCustomers(DiscountCode discountCode,ArrayList<String> customers) throws AccountsException {
+        for (String customer : customers) {
+            User user = User.getUserByUsername(customer);
+            if(!(user instanceof Customer))
+                throw new AccountsException("You can just add customers.");
+            discountCode.addCostumer((Customer)user);
+        }
+    }
+    public void createDiscount(String startDate, String endDate, int percentage, double maxDiscount, int countForEachUser,
+                              ArrayList<String> listOfUsernames) throws AccountsException {
+        SimpleDateFormat startFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+        SimpleDateFormat endFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+        Date start;
+        Date end;
+        try {
+            start = startFormat.parse(startDate);
+            end = endFormat.parse(endDate);
+        }
+        catch (Exception e){
+            throw new AccountsException("Invalid date format");
+        }
+        DiscountCode discountCode = new DiscountCode(start, end, percentage, maxDiscount, countForEachUser);
+        if(discountCode.isExpired())
+            throw new AccountsException("Invalid date");
+        addCustomers(discountCode, listOfUsernames);
     }
 
     public ArrayList<String> getAllDiscountCodes(){
