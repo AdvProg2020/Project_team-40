df6f403d32ca47dbe67232c7bfcc7a8c29d7c4f8
soligits <m.soltani.252@gmail.com>
2020-04-29 15:40:15 +0430

    load and save completed. added new mothods to off.

--- src/main/java/model/Off.java
+++ src/main/java/model/Off.java
@@ -3,11 +3,15 @@ package model;
 import model.enumerations.SetUpStatus;
 import model.users.Seller;
 
+import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 
-public class Off {
+public class Off implements Serializable{
+    private static final String PATH = "src/main/resources/offs/";
     private static HashMap<String, Off> allOffs;
     private ArrayList<String> productIDs;
     private String id;
@@ -15,9 +19,9 @@ public class Off {
     private String startDate;
     private String endDate;
     private String seller;
-    private int discountPercentage;
+    private double discountPercentage;
 
-    public Off(String startDate, String endDate, int discountPercentage, String seller) {
+    public Off(String startDate, String endDate, double discountPercentage, String seller) {
         this.startDate = startDate;
         this.endDate = endDate;
         this.discountPercentage = discountPercentage;
@@ -37,7 +41,7 @@ public class Off {
         this.endDate = endDate;
     }
 
-    public void setDiscountPercentage(int discountPercentage) {
+    public void setDiscountPercentage(double discountPercentage) {
         this.discountPercentage = discountPercentage;
     }
 
@@ -57,8 +61,40 @@ public class Off {
         return productIDs;
     }
 
-    public String getSeller() {
-        return seller;
+    public SetUpStatus getStatus(){
+        return status;
+    }
+
+    public String getStartDate(){
+        return startDate;
+    }
+
+    public String getEndDate(){
+        return endDate;
+    }
+
+    public double getDiscountPercentage(){
+        return discountPercentage;
+    }
+
+    public boolean isAvailable(){
+
+        Date now = new Date();
+        Date start = new Date();
+        Date end = new Date();
+        try{
+            start = new SimpleDateFormat("dd/MM/yy HH:mm:ss").parse(startDate);
+            end = new SimpleDateFormat("dd/MM/yy HH:mm:ss").parse(endDate);
+        }catch(ParseException e){
+          e.printStackTrace();
+        };
+
+
+        return status.equals(SetUpStatus.Confirmed) && now.compareTo(start) > 0 && now.compareTo(end) < 0;
+    }
+
+    public Seller getSeller() {
+        return (Seller)Seller.getUserByUsername(seller);
     }
 
     public static HashMap<String, Off> getAllOffs() {
@@ -73,8 +109,40 @@ public class Off {
         allOffs.put(off.getId(), off);
     }
 
-    public static void loadData(){}
+    public static void loadData() throws IOException{
+        File directory = new File(PATH);
+        String[] pathNames = directory.list();
+        assert pathNames != null;
+        for (String path: pathNames) {
+            FileInputStream file = new FileInputStream(PATH + path);
+            ObjectInputStream inputStream = new ObjectInputStream(file);
+            try {
+                allOffs.put(((Off)inputStream.readObject()).getId(), (Off) inputStream.readObject());
+                file.close();
+                inputStream.close();
+                new File(PATH + path).delete();
+                //TODO:IMPLEMENT PROPER EXCEPTION
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
 
-    public static void saveData(){}
+        }
+    }
+
+    public static void saveData(){
+        for (Off off : allOffs.values()) {
+            try {
+                FileOutputStream file = new FileOutputStream(PATH + off.getId());
+                ObjectOutputStream outputStream = new ObjectOutputStream(file);
+                outputStream.writeObject(off);
+                file.close();
+                outputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 
 }
