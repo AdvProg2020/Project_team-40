b1545e6626a979cfb5c21be813f56f03fb5bec3e
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-05-01 17:34:02 +0430

    Completed EditProduct class and related methods

--- src/main/java/model/requests/EditProduct.java
+++ src/main/java/model/requests/EditProduct.java
@@ -1,22 +1,52 @@
 package model.requests;
 
+import Controller.Accounts.SellerAccountController;
 import model.Product;
+import model.enumerations.SetUpStatus;
 
 public class EditProduct extends Request{
+    private Product product;
+    private String field;
+    private String newField;
 
-
-    @Override
-    protected void generateId() {
-
+    public EditProduct(String productId, String field, String newField) {
+        super("Edit Product");
+        this.product = Product.getProductById(productId);
+        this.field = field;
+        this.newField = newField;
     }
 
     @Override
     public void action() {
+        if(isAccepted) {
+            editProductAfterManagersAcceptance();
+        }
+    }
 
+    public void editProductAfterManagersAcceptance(){
+        if(field.equals("name")) {
+            product.setName(newField);
+        } else if(field.equalsIgnoreCase("company")) {
+            product.setCompany(newField);
+        } else if(field.equalsIgnoreCase("price")) {
+            product.setPrice(Double.parseDouble(newField));
+        } else if(field.equalsIgnoreCase("count")) {
+            product.setCount(Integer.parseInt(newField));
+        } else if(field.equalsIgnoreCase("category")) {
+            product.setCategory(newField);
+        } else if(field.equalsIgnoreCase("status")) {
+            if(newField.equalsIgnoreCase("creating")) {
+                product.setStatus(SetUpStatus.Creating);
+            } else if(newField.equalsIgnoreCase("editing")) {
+                product.setStatus(SetUpStatus.Editing);
+            } else if(newField.equalsIgnoreCase("confirmed")) {
+                product.setStatus(SetUpStatus.Confirmed);
+            }
+        }
     }
 
     @Override
     public String toString() {
-        return null;
+        return super.toString() + "Edited product: " + product;
     }
 }
