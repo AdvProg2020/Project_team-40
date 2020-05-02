425774804d7dd99ad7b714915ab29d5964461157
mtndaghyani <matin.daghyani@gmail.com>
2020-05-02 19:10:42 +0430

    Implement manage users method

--- src/main/java/View/AccountMenus/ManagrView/ManagersAccount.java
+++ src/main/java/View/AccountMenus/ManagrView/ManagersAccount.java
@@ -1,12 +1,35 @@
 package View.AccountMenus.ManagrView;
 
 import Controller.Accounts.ManagerAccountController;
+import View.AccountMenus.PeopleAccountMenu;
 import View.Menu;
 
-public class ManagersAccount extends Menu {
-    ManagerAccountController managerAccountController;
+import java.util.HashMap;
+
+public class ManagersAccount extends PeopleAccountMenu {
+    private ManagerAccountController managerAccountController;
 
     public ManagersAccount(Menu parentMenu) {
         super("Manager Account", parentMenu);
+        HashMap<Integer, Menu>submenus = new HashMap<>();
+        submenus.put(1, new ManageUsersMenu(this));
+        this.setSubMenus(submenus);
+    }
+    public void show(){
+        System.out.println("List of users:");
+        for (String userName : managerAccountController.getAllUserNames()) {
+            System.out.println(userName);
+        }
+        super.show();
+    }
+
+    @Override
+    public void getShowInfo() {
+
+    }
+
+    @Override
+    public void getEditInfo() {
+
     }
 }
