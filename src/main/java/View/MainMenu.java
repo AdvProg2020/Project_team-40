fcc1544f4f213efba17a1a8e043e1fe299547c86
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-05-02 19:24:54 +0430

    Completed registerOrLogin method in MainMenu

--- src/main/java/View/MainMenu.java
+++ src/main/java/View/MainMenu.java
@@ -3,6 +3,8 @@ package View;
 import Controller.Accounts.CustomerAccountController;
 import Controller.Accounts.ManagerAccountController;
 import Controller.Accounts.SellerAccountController;
+import View.ShopingMenus.ProductsAndOffsMenus.OffsMenu;
+import View.ShopingMenus.ProductsAndOffsMenus.ProductsMenu;
 
 import java.util.HashMap;
 
@@ -14,11 +16,50 @@ public class MainMenu extends Menu{
     public MainMenu( Menu parentMenu) {
         super("Main Menu", parentMenu);
         HashMap<Integer, Menu> subMenus = new HashMap<>();
+        subMenus.put(2, new ProductsMenu(this));
+        subMenus.put(3, new OffsMenu(this));
         this.setSubMenus(subMenus);
     }
 
     public Menu getRegisterOrLogin() {
-        return null;
+        return new Menu("Register And Login", this) {
+            @Override
+            public void show() {
+                System.out.println("1. Register\n" +
+                        "2. Login\n" +
+                        "3. Back");
+            }
+
+            @Override
+            public void execute() {
+                int chosenNumber = getNumberOfNextMenu();
+                if(chosenNumber == 1) {
+                    getRegister();
+                } else if(chosenNumber == 2) {
+                    getLogin();
+                } else if(chosenNumber == 3) {
+                    parentMenu.show();
+                    parentMenu.execute();
+                }
+            }
+
+            @Override
+            public int getNumberOfNextMenu() {
+                int chosenMenu;
+                while(true) {
+                    while (!ConsoleCommand.INTEGER.getStringMatcher(input = scanner.nextLine().trim()).matches()) {
+                        System.out.println("Please write the number of one of the options.");
+                    }
+                    chosenMenu = Integer.parseInt(input);
+                    if(chosenMenu <= 3) {
+                        break;
+                    } else {
+                        System.out.println("Please write the number of one of the options.");
+                    }
+                }
+                return chosenMenu;
+            }
+        };
     }
 
     public Menu getRegister() {
