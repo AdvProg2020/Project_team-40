cd963376de6af791f05dc3856bcd2bc8c90ef150
mtndaghyani <matin.daghyani@gmail.com>
2020-05-08 17:22:33 +0430

    Complete ManagersAccount menu

--- src/main/java/View/AccountMenus/ManagrView/ManagersAccount.java
+++ src/main/java/View/AccountMenus/ManagrView/ManagersAccount.java
@@ -1,18 +1,20 @@
 package View.AccountMenus.ManagrView;
 
-import Controller.Accounts.ManagerAccountController;
 import View.AccountMenus.PeopleAccountMenu;
 import View.Menu;
 
 import java.util.HashMap;
 
 public class ManagersAccount extends PeopleAccountMenu {
-    private ManagerAccountController managerAccountController;
 
     public ManagersAccount(Menu parentMenu) {
         super("Manager Account", parentMenu);
         HashMap<Integer, Menu>submenus = new HashMap<>();
         submenus.put(1, new ManageUsersMenu(this));
+        submenus.put(2, new ManageProductsMenu(this));
+        submenus.put(3, new DiscountsMenu(this));
+        submenus.put(4, new RequestMenu(this));
+        submenus.put(5, new CategoryMenu(this));
         this.setSubMenus(submenus);
     }
 
