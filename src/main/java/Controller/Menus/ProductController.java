3b0bf40c06cfd341b2395c1dbfb62db97f4c3384
soligits <m.soltani.252@gmail.com>
2020-05-09 21:10:35 +0430

    ProductController completed.

--- src/main/java/Controller/Menus/ProductController.java
+++ src/main/java/Controller/Menus/ProductController.java
@@ -1,37 +1,17 @@
 package Controller.Menus;
 
+import exceptions.MenuException;
+import model.Cart;
 import model.Comment;
 import model.Product;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ProductController{
 
     public static ProductController productController;
-
-    public Product digestProduct(String productID){
-        return null;
-    }
-
-    public void addProductToCart(String productID){
-
-    }
-
-    public void sellectSellerForProduct(String productID, String sellerUsername){
-
-    }
-
-    public ArrayList<String> getProductAttributes(String productID){
-        return null;
-    }
-
-    public ArrayList<Comment> getComment(String productID){
-        return null;
-    }
-
-    public void addComment(String productID, String title, String content){
-
-    }
+    private String productName;
+    private Product productChosen;
 
     private ProductController(){
 
@@ -44,4 +24,96 @@ public class ProductController{
         return productController;
     }
 
+    public Product digestProduct(String productID) throws MenuException{
+        Product product = Product.getProductById(productID);
+        if(product == null)
+            throw new MenuException("No product with such name exists.");
+        return product;
+    }
+
+    public void addProductToCart(String productID, int count) throws MenuException{
+        Product product = Product.getProductById(productID);
+        if(product == null)
+            throw new MenuException("No product with such name exists.");
+
+        if(count < product.getCount())
+            throw new MenuException("Not enough goods available in stock.");
+
+        Cart.addProduct(productID, count);
+    }
+
+    public void sellectSellerForProduct(String productName, String sellerUsername) throws MenuException{
+        ArrayList<Product> products = new ArrayList<>();
+        for(Product product : Product.getAllProducts().values()) {
+            if(product.getName().equals(productName))
+                products.add(product);
+        }
+
+        if(products.size() == 0)
+            throw new MenuException("No product with such name exists");
+
+        Product productSelected = null;
+
+        for(Product product : products) {
+            if(product.getSeller().getUsername().equals(sellerUsername))
+                productSelected = product;
+        }
+
+        if(productSelected == null)
+            throw new MenuException("This seller doesn't offer product with this name.");
+
+        productChosen = productSelected;
+    }
+
+    public Product getProductChosen(){
+        return productChosen;
+    }
+
+    public ArrayList<String> getProductAttributes(String productID) throws MenuException{
+        Product product = Product.getProductById(productID);
+
+        if(product == null)
+            throw new MenuException("No product with such name exists.");
+
+        ArrayList<String> productAttributes = new ArrayList<>();
+        productAttributes.addAll(Arrays.asList(
+                "Seller",
+                "Name",
+                "Price",
+                "Status",
+                "Company",
+                "Category",
+                "Explanation"
+        ));
+
+        for(String name : product.getExtraStringProperties().keySet()) {
+            productAttributes.add(name);
+        }
+
+        for(String name : product.getExtraValueProperties().keySet()) {
+            productAttributes.add(name);
+        }
+
+        return productAttributes;
+    }
+
+    public ArrayList<Comment> getComments(String productID) throws MenuException{
+        Product product = Product.getProductById(productID);
+
+        if(product == null)
+            throw new MenuException("No product with such name exists.");
+
+        return product.getComments();
+    }
+
+    public void addComment(String productID, String title, String content) throws MenuException{
+        Product product = Product.getProductById(productID);
+
+        if(product == null)
+            throw new MenuException("No product with such name exists.");
+        //TODO : add all arguments
+        Comment comment = new Comment("", productID, title + "\n" + content, false, false, null, null);
+
+        product.addComment(comment);
+    }
 }
