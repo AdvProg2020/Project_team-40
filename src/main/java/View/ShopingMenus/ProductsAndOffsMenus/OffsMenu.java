be1f59e21585e6cf81348e7cf59140e0edc3dad9
mtndaghyani <matin.daghyani@gmail.com>
2020-05-10 23:35:27 +0430

    Complete off menu

--- src/main/java/View/ShopingMenus/ProductsAndOffsMenus/OffsMenu.java
+++ src/main/java/View/ShopingMenus/ProductsAndOffsMenus/OffsMenu.java
@@ -2,19 +2,50 @@ package View.ShopingMenus.ProductsAndOffsMenus;
 
 import Controller.Menus.OffMenuController;
 import View.Menu;
+import model.Off;
+import model.Product;
+
+import javax.crypto.spec.PSource;
+import java.util.HashMap;
 
 public class OffsMenu extends Menu {
-    OffMenuController offMenuController;
+    private OffMenuController offMenuController;
 
     public OffsMenu(Menu parentMenu) {
         super("Offs Menu", parentMenu);
+        offMenuController = OffMenuController.getInstance();
+        HashMap<Integer, Menu> submenus = new HashMap<>();
+        submenus.put(1, getShowProductsWithOff());
+        submenus.put(2, getShowProduct());
+        submenus.put(3, new SortsMenu<>(this, offMenuController));
+        submenus.put(4, new FiltersMenu<>(this, offMenuController));
+        setSubMenus(submenus);
+
     }
 
-    public void getShowProductsWithOff(){
+    public Menu getShowProductsWithOff(){
+        return new Menu("Show products with off", this) {
+            @Override
+            public void show() {
+                for (Product product : offMenuController.getProductsWithOff()) {
+                    System.out.println(product);
+                }
+                System.out.println("Enter anything to return");
+            }
 
+            @Override
+            public void execute() {
+                scanner.nextLine();
+                this.parentMenu.show();
+                this.parentMenu.execute();
+            }
+        };
     }
 
-    public void getShowProduct(){
-
+    public Menu getShowProduct(){
+        //TODO:Implement
+        return null;
+        }
     }
-}
+
+
