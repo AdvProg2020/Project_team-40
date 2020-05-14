2e96d39aa93e1fbc4ac8da7cf38dbf8381aec602
mtndaghyani <matin.daghyani@gmail.com>
2020-05-14 23:35:34 +0430

    Handle invalid inputs exeption

--- src/main/java/View/AccountMenus/ManagrView/DiscountsMenu.java
+++ src/main/java/View/AccountMenus/ManagrView/DiscountsMenu.java
@@ -1,9 +1,11 @@
 package View.AccountMenus.ManagrView;
 
 import Controller.Accounts.ManagerAccountController;
+import View.ConsoleCommand;
 import View.Menu;
 import exceptions.AccountsException;
 import model.DiscountCode;
+import model.users.User;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,21 +17,33 @@ public class DiscountsMenu extends Menu {
         super("Manage Discounts Menu", parentMenu);
         managerAccountController = ManagerAccountController.getInstance();
         HashMap<Integer, Menu> submenus = new HashMap<>();
-        submenus.put(1, getCreateDiscount());
-        submenus.put(2, getViewDiscount());
-        submenus.put(3, getEditDiscount());
-        submenus.put(4, getRemoveDiscount());
+        submenus.put(1, getShowAllDiscountCodes());
+        submenus.put(2, getCreateDiscount());
+        submenus.put(3, getViewDiscount());
+        submenus.put(4, getEditDiscount());
+        submenus.put(5, getRemoveDiscount());
         setSubMenus(submenus);
     }
 
-    @Override
-    public void show() {
-        for (DiscountCode discountCode : managerAccountController.getAllDiscountCodes()) {
-            System.out.println(discountCode);
-            System.out.println("--------------------------------------------------");
-        }
-        System.out.println("=====================================================");
-        super.show();
+    public Menu getShowAllDiscountCodes(){
+        return new Menu("View all disount codes", this) {
+            @Override
+            public void show() {
+                for (DiscountCode discountCode : managerAccountController.getAllDiscountCodes()) {
+                    System.out.println(discountCode);
+                    System.out.println("--------------------------------------------------");
+                }
+                System.out.println("=====================================================");
+                System.out.println("Enter anything to return");
+            }
+
+            @Override
+            public void execute() {
+                scanner.nextLine();
+                this.parentMenu.show();
+                this.parentMenu.execute();
+            }
+        };
     }
 
     public Menu getCreateDiscount(){
@@ -44,16 +58,17 @@ public class DiscountsMenu extends Menu {
                 System.out.println("Enter ending date in this format: dd-MM-yyyy HH:mm");
                 String end = scanner.nextLine();
                 System.out.println("Enter discount percentage: ");
-                int percentage = Integer.parseInt(scanner.nextLine());
+                int percentage = Integer.parseInt(getValidInput(ConsoleCommand.INTEGER, "You can just use NUMERIC characters!"));
                 System.out.println("Enter maximum discount amount for a product: ");
-                double max = Double.parseDouble(scanner.nextLine());
+                double max = Double.parseDouble(getValidInput(ConsoleCommand.DOUBLE, "You can just use NUMERIC characters!"));
                 System.out.println("Enter number of codes per user: ");
-                int count = Integer.parseInt(scanner.nextLine());
+                int count = Integer.parseInt(getValidInput(ConsoleCommand.INTEGER, "You can just use NUMERIC characters!"));
                 System.out.println("Enter included usernames. Enter OK to end.");
                 ArrayList <String> allUsernames = new ArrayList<>();
                 String username;
                 while (!(username = scanner.nextLine()).equalsIgnoreCase("Ok")){
-                    allUsernames.add(username);
+                    if (User.doesUserExist(username))
+                        allUsernames.add(username);
                 }
                 try {
                     managerAccountController.createDiscount(start, end, percentage, max, count, allUsernames);
