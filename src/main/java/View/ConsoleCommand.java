6e7038f3d37376b468128143e4dea5e3b3c26ead
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-05-16 13:59:24 +0430

    Small adjustments in regex

--- src/main/java/View/ConsoleCommand.java
+++ src/main/java/View/ConsoleCommand.java
@@ -7,7 +7,7 @@ public enum ConsoleCommand {
     INTEGER("\\d+"),
     PHONE_NUMBER("\\d+"),
     EMAIL_ADDRESS(".+@\\w+\\.\\w+"),
-    NAME("^[a-z]+$"),
+    NAME("^[a-zA-Z]+$"),
     DOUBLE("\\d+\\.{0,1}\\d*"),
     DATE("^\\d{2}\\/\\d{2}\\/\\d{2}\\s[0-2]\\d:[0-5]\\d:[0-5]\\d$"),
     DEFAULT(".+");
