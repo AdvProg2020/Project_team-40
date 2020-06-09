310ceccfd0e494c5b9d6337289b93565fcd4af4a
mtndaghyani <matin.daghyani@gmail.com>
2020-06-10 02:14:47 +0430

    Add top menu bar to Manager Account menu

--- src/main/java/View/AccountMenus/ManagerView/AccountView/AccountManager.java
+++ src/main/java/View/AccountMenus/ManagerView/AccountView/AccountManager.java
@@ -1,4 +1,44 @@
 package View.AccountMenus.ManagerView.AccountView;
 
-public class AccountManager {
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AccountManager implements Initializable {
+    public JFXButton homeButton;
+    public Pane innerPane;
+
+    public void setInnerPane(Parent root){
+        innerPane.getChildren().clear();
+        innerPane.getChildren().add(root);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/Main/PersonalInfo.fxml"));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        setInnerPane(root);
+    }
+
+
+    public void goToHomeMenu(ActionEvent event) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("PersonalInfo.fxml"));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        setInnerPane(root);
+    }
 }
