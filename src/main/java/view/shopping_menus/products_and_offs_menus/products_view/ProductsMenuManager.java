8015ec3369dd803ecc1ebcd022a19afa719b9962
soligits <m.soltani.252@gmail.com>
2020-06-11 14:18:45 +0430

    Updated products_menu.fxml and ProductsMenuManager.java

--- src/main/java/view/shopping_menus/products_and_offs_menus/products_view/ProductsMenuManager.java
+++ src/main/java/view/shopping_menus/products_and_offs_menus/products_view/ProductsMenuManager.java
@@ -1,4 +1,24 @@
 package view.shopping_menus.products_and_offs_menus.products_view;
 
-public class ProductsMenuManager {
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXScrollPane;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.VBox;
+import view.MenuManager;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProductsMenuManager extends MenuManager implements Initializable{
+    public ChoiceBox sortChoiceBox;
+    public VBox productsVBox;
+    public JFXScrollPane productsScrollPane;
+    public JFXButton showMoreButton;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+    }
 }
