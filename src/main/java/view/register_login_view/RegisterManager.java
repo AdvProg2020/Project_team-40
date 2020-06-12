99a29f33f873a7c72d5decaf09d6cad196b9681f
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-06-12 12:22:18 +0430

    Implemented register method in RegisterManager

--- src/main/java/view/register_login_view/RegisterManager.java
+++ src/main/java/view/register_login_view/RegisterManager.java
@@ -1,11 +1,15 @@
 package view.register_login_view;
 
 import controller.accounts.AccountController;
+import controller.accounts.CustomerAccountController;
+import controller.accounts.SellerAccountController;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import model.users.Customer;
 import model.users.User;
 import view.MenuManager;
 
@@ -19,12 +23,45 @@ public class RegisterManager extends MenuManager implements Initializable {
     public RadioButton managerButton;
     public Label creditLabel;
     public Label companyLabel;
-    public TextField lastName;
+    public Label usernameError;
+    public Label passwordError;
+    public Label firstNameError;
+    public Label lastNameError;
+    public Label emailError;
+    public Label phoneNumberError;
+    public Label creditError;
+    public Label companyError;
     public TextField companyField;
     public TextField creditField;
+    public TextField username;
+    public PasswordField password;
+    public TextField firstName;
+    public TextField lastName;
+    public TextField email;
+    public TextField phoneNumber;
 
     public void register() {
 
+        if(customerButton.isPressed()) {
+            registerCustomer();
+        } else if(sellerButton.isPressed()) {
+            registerSeller();
+        } else if(managerButton.isPressed()) {
+            registerManager();
+        }
+    }
+
+    private void registerManager() {
+
+    }
+
+    private void registerSeller() {
+
+    }
+
+    private void registerCustomer() {
+        CustomerAccountController.getInstance().createCustomerAccount(username.getText(), password.getText(),
+                firstName.getText(), lastName.getText(), email.getText(), phoneNumber.getText(), creditField.getText());
     }
 
     public void clickCustomer() {
