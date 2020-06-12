08143df3c82572b6d0977eef643eaf3292b3d3bc
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-06-12 11:41:05 +0430

    Created Toggle Group for roles in register

--- src/main/java/view/register_login_view/RegisterManager.java
+++ src/main/java/view/register_login_view/RegisterManager.java
@@ -1,10 +1,32 @@
 package view.register_login_view;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import view.MenuManager;
 
-public class RegisterManager {
-
+public class RegisterManager extends MenuManager {
+    public RadioButton customerButton;
+    public RadioButton sellerButton;
+    public RadioButton managerButton;
+    public Label creditLabel;
+    public Label companyLabel;
 
     public void register() {
 
+    }
+
+    public void clickCustomer() {
+        creditLabel.setText("Credit:");
+        companyLabel.setText("");
+    }
+
+    public void clickSeller() {
+        companyLabel.setText("Company:");
+        creditLabel.setText("Credit:");
+    }
+
+    public void clickManager() {
+        companyLabel.setText("");
+        creditLabel.setText("");
     }
 }
