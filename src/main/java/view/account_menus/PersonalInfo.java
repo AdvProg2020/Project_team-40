0a2f7a8ba7ac1606d6ca135f6e8c67d2115dc369
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-06-14 10:43:00 +0430

    Changed initializer

--- src/main/java/view/account_menus/PersonalInfo.java
+++ src/main/java/view/account_menus/PersonalInfo.java
@@ -6,6 +6,8 @@ import controller.accounts.SellerAccountController;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import model.users.Customer;
@@ -23,12 +25,24 @@ public class PersonalInfo extends AccountMenu implements Initializable {
     public Label credit;
     public Label company;
     public Label companyLabel;
+    public Label username;
+    public Label firstName;
+    public Label lastName;
+    public Label email;
+    public Label phone;
     public Button editUsername;
     public Button editFirstName;
     public Button editLastName;
     public Button editPhoneNumber;
     public Button editEmail;
     public Button editCompany;
+    public TextField newUsername;
+    public TextField newFirstName;
+    public TextField newLastName;
+    public TextField newEmail;
+    public TextField newPhoneNumber;
+    public TextField newCredit;
+    public TextField newCompany;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -44,5 +58,39 @@ public class PersonalInfo extends AccountMenu implements Initializable {
             editCompany = new Button("edit");
             gridPane.add(editCompany, 3, 6);
         }
+        username.setText(user.getUsername());
+        firstName.setText(user.getFirstName());
+        lastName.setText(user.getLastName());
+        email.setText(user.getEmail());
+        phone.setText(user.getPhoneNo());
+    }
+
+    public void editUsername() {
+        editUsername.setText("save");
+        editUsername.setOnMouseClicked(e -> saveUsername());
+        if(newUsername == null)
+            newUsername = new TextField();
+        gridPane.add(newUsername, 1, 0);
+    }
+
+    private void saveUsername() {
+        accountController.editUser(newUsername.getText(), "username");
+        gridPane.getChildren().remove(newUsername);
+        newUsername.setText("");
+        editUsername.setOnMouseClicked(e -> editUsername());
+        editUsername.setText("edit");
+    }
+
+    public void editFirstName() {
+
+    }
+
+    public void editLastName() {
+    }
+
+    public void editEmail() {
+    }
+
+    public void editPhoneNumber() {
     }
 }
