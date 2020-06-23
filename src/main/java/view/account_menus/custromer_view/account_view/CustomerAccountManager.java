e034ca784d9919b486fad7282d4d23b3b6031ee5
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-06-23 17:35:47 +0430

    Created proper menu bar for Customer

--- src/main/java/view/account_menus/custromer_view/account_view/CustomerAccountManager.java
+++ src/main/java/view/account_menus/custromer_view/account_view/CustomerAccountManager.java
@@ -1,8 +1,22 @@
 package view.account_menus.custromer_view.account_view;
 
+import javafx.scene.input.MouseEvent;
 import view.account_menus.AllAccountsManager;
 
 public class CustomerAccountManager extends AllAccountsManager {
 
 
+    public void goToHome() {
+
+    }
+
+    public void goToOrders() {
+
+    }
+
+    public void goToCart() {
+    }
+
+    public void goToDiscountCode() {
+    }
 }
---
+++ src/main/java/view/account_menus/custromer_view/cart_view/Payment.java
@@ -0,0 +1,7 @@
+package view.account_menus.custromer_view.cart_view;
+
+import view.account_menus.AllAccountsManager;
+
+public class Payment extends AllAccountsManager {
+    
+}
