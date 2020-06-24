4afc7273f66d5c1d1f6a2cc66aa39fab4175d77a
mtndaghyani <matin.daghyani@gmail.com>
2020-06-24 19:42:36 +0430

    Fix logout process bug and fix setInnerPane overloading problem

--- src/main/java/view/account_menus/customer_view/account_view/CustomerAccountManager.java
+++ src/main/java/view/account_menus/customer_view/account_view/CustomerAccountManager.java
@@ -1,6 +1,7 @@
 package view.account_menus.customer_view.account_view;
 
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
 import view.account_menus.AllAccountsManager;
 
 import java.net.URL;
@@ -8,8 +9,11 @@ import java.util.ResourceBundle;
 
 public class CustomerAccountManager extends AllAccountsManager implements Initializable {
 
+    public AnchorPane secondaryInnerPane;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        setSecondaryPane(secondaryInnerPane);
         setSecondaryInnerPane("/layouts/accounts_menu.fxml");
     }
 
