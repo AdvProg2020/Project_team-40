cbe0d3831c1d2e7aab0db7472386c1ec63cc0ef9
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-06-24 09:00:53 +0430

    Partly implemented products_menu_design.fxml

--- src/main/java/view/account_menus/seller_view/sellers_products_view/SellersProductsManager.java
+++ src/main/java/view/account_menus/seller_view/sellers_products_view/SellersProductsManager.java
@@ -1,7 +1,13 @@
 package view.account_menus.seller_view.sellers_products_view;
 
+import javafx.event.ActionEvent;
 import view.MenuManager;
 
 public class SellersProductsManager extends MenuManager {
 
+    public void handleRefresh() {
+    }
+
+    public void handleAddCategory() {
+    }
 }
