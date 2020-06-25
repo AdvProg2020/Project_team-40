b5bd49da43b61fc49ae485767d3129946b374064
soligits <m.soltani.252@gmail.com>
2020-06-25 23:17:00 +0430

    Updated digest menu.

--- src/main/java/view/shopping_menus/product/product_view/ProductMenuManager.java
+++ src/main/java/view/shopping_menus/product/product_view/ProductMenuManager.java
@@ -1,13 +1,24 @@
 package view.shopping_menus.product.product_view;
 
+import com.jfoenix.controls.JFXTreeTableColumn;
+import com.jfoenix.controls.JFXTreeTableView;
+import com.jfoenix.controls.RecursiveTreeItem;
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import controller.menus.ProductController;
 import exceptions.MenuException;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.util.Callback;
 import model.Cart;
 import model.Comment;
 import model.Product;
@@ -15,6 +26,8 @@ import view.MenuManager;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class ProductMenuManager extends MenuManager implements Initializable{
@@ -73,7 +86,77 @@ public class ProductMenuManager extends MenuManager implements Initializable{
     }
 
     private void initializeDigest(){
+        digestSection.setSpacing(20);
+
+        //add description
+        Text title = new Text("Description :");
+        title.setStyle("-fx-font-size: 28");
+        digestSection.getChildren().add(title);
         digestSection.getChildren().add(new Text(product.getExplanation()));
+
+        //add attributes
+        Text title2 = new Text("Attributes :");
+        title2.setStyle("-fx-font-size: 28");
+        digestSection.getChildren().add(title2);
+
+        JFXTreeTableView<Attribute> treeView = new JFXTreeTableView<>();
+        digestSection.getChildren().add(treeView);
+
+        JFXTreeTableColumn<Attribute, String> nameCol = new JFXTreeTableColumn<>("Attribute name");
+        nameCol.setPrefWidth(400);
+        nameCol.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<Attribute, String>, ObservableValue<String>>(){
+            @Override
+            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<Attribute, String> param){
+                return param.getValue().getValue().name;
+            }
+        });
+
+        JFXTreeTableColumn<Attribute, String> valueCol = new JFXTreeTableColumn<>("Attribute value");
+        valueCol.setPrefWidth(400);
+        valueCol.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<Attribute, String>, ObservableValue<String>>(){
+            @Override
+            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<Attribute, String> param){
+                return param.getValue().getValue().value;
+            }
+        });
+
+        ObservableList<Attribute> attributes = FXCollections.observableArrayList();
+        try {
+            HashMap<String, String> attributesHashMap = ProductController.getInstance().getProductAttributes(productID);
+            for(Map.Entry<String, String> entry : attributesHashMap.entrySet()) {
+                Attribute attribute = new Attribute(entry.getKey(), entry.getValue());
+                attributes.add(attribute);
+                System.out.println(attribute);
+            }
+        } catch(MenuException e) {
+            e.printStackTrace();
+        }
+
+        final TreeItem<Attribute> root = new RecursiveTreeItem<>(attributes, RecursiveTreeObject::getChildren);
+        treeView.getColumns().setAll(nameCol, valueCol);
+        treeView.setRoot(root);
+        treeView.setShowRoot(false);
+
+    }
+
+    class Attribute extends RecursiveTreeObject<Attribute> {
+        StringProperty name;
+        StringProperty value;
+
+        public Attribute(String name, String value){
+            this.name = new SimpleStringProperty(name);
+            if(value == null)
+                value = "";
+            this.value = new SimpleStringProperty(value);
+        }
+
+        @Override
+        public String toString(){
+            return "Attribute{" +
+                    "name=" + name +
+                    ", value=" + value +
+                    '}';
+        }
     }
 
     private void initializeComments(){
