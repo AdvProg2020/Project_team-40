b550b283b6a0383db82dfe72d70cc0fa53c311b1
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-06-25 16:48:49 +0430

    Completed add_product.fxml

--- src/main/java/view/account_menus/seller_view/sellers_products_view/AddProductManager.java
+++ src/main/java/view/account_menus/seller_view/sellers_products_view/AddProductManager.java
@@ -3,4 +3,8 @@ package view.account_menus.seller_view.sellers_products_view;
 import view.MenuManager;
 
 public class AddProductManager extends MenuManager {
+
+    public void createProduct() {
+
+    }
 }
