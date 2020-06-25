be243b91e43903f9e11a390db25775440616f4ba
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-06-25 19:27:49 +0430

    Implemented createProduct method in add_product.fxml

--- src/main/java/view/account_menus/seller_view/sellers_products_view/AddProductManager.java
+++ src/main/java/view/account_menus/seller_view/sellers_products_view/AddProductManager.java
@@ -3,13 +3,14 @@ package view.account_menus.seller_view.sellers_products_view;
 import com.jfoenix.controls.JFXTextField;
 import controller.accounts.SellerAccountController;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
 import model.Category;
 import model.enumerations.PropertyType;
 import view.MenuManager;
+import view.ValidInput;
 
 import java.net.URL;
 import java.util.HashMap;
@@ -17,7 +18,6 @@ import java.util.ResourceBundle;
 import java.util.Set;
 
 public class AddProductManager extends MenuManager implements Initializable {
-    public VBox informationBox;
     private SellerAccountController sellerAccountController;
     private ToggleGroup toggleGroupCategory;
     private HashMap<String, Double> extraValueProperties;
@@ -25,6 +25,16 @@ public class AddProductManager extends MenuManager implements Initializable {
     private HashMap<String, JFXTextField> valuePropertyField;
     private HashMap<String, JFXTextField> stringPropertyField;
     public VBox propertyBox;
+    public VBox informationBox;
+    public Label categoryError;
+    public JFXTextField nameField;
+    public JFXTextField companyField;
+    public JFXTextField priceField;
+    public JFXTextField countField;
+    public Label nameError;
+    public Label companyError;
+    public Label priceError;
+    public Label countError;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -37,11 +47,13 @@ public class AddProductManager extends MenuManager implements Initializable {
             toggleGroupCategory.getToggles().add(categoryButton);
             informationBox.getChildren().add(categoryButton);
         }
+
     }
 
     private void showCategoryProperties(RadioButton radioButton) {
         HashMap<String, PropertyType> categoryProperties = sellerAccountController.
                 getCategoryProperties(radioButton.getText());
+        propertyBox.getChildren().clear();
         extraStringProperties = new HashMap<>();
         extraValueProperties = new HashMap<>();
         valuePropertyField = new HashMap<>();
@@ -59,6 +71,43 @@ public class AddProductManager extends MenuManager implements Initializable {
         }
     }
 
+    public void createProduct() {
+        if(valuePropertyField == null) {
+            categoryError.setText("Choose a category!");
+        } else if(isAFieldEmpty()) {
+            categoryError.setText("Fill all fields!");
+        } else if(nameField.getText().isBlank()) {
+            nameError.setText("Enter name!");
+        } else if(companyField.getText().isBlank()) {
+            companyError.setText("Enter company!");
+        } else if(priceField.getText().isBlank()) {
+            priceError.setText("Enter price!");
+        } else if(countField.getText().isBlank()) {
+            countError.setText("Enter count!");
+        } else if(!ValidInput.INTEGER.getStringMatcher(countField.getText()).matches()) {
+            countError.setText("Enter a number!");
+        } else if(!ValidInput.DOUBLE.getStringMatcher(priceField.getText()).matches()) {
+            priceError.setText("Enter a number!");
+        } else {
+            finalizeCreatingProduct();
+        }
+    }
 
-    public void createProduct() {}
+    private void finalizeCreatingProduct() {
+
+    }
+
+    private boolean isAFieldEmpty() {
+        boolean isEmpty = false;
+        for(JFXTextField field : valuePropertyField.values()) {
+            if(field.getText().isBlank())
+                isEmpty = true;
+        }
+
+        for(JFXTextField field : stringPropertyField.values()) {
+            if(field.getText().isBlank())
+                isEmpty = true;
+        }
+        return isEmpty;
+    }
 }
