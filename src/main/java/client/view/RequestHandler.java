54bb904d7fa2f79a3f6395b402cd6bd67ef8ebf1
mtndaghyani <matin.daghyani@gmail.com>
2020-07-10 14:48:39 +0430

    Add PUT method to RequestHandler

--- src/main/java/client/view/RequestHandler.java
+++ src/main/java/client/view/RequestHandler.java
@@ -27,4 +27,14 @@ public class RequestHandler {
         }
         return clientResource.get(outputClass);
     }
+    public static <T> Object put(String path, Object entity, HashMap<String, String> queries, boolean doesSendToken, Class<T> outputClass) {
+        ClientResource clientResource = new ClientResource(ENDPOINT + path);
+        if (doesSendToken)
+            clientResource.setQueryValue("auth-token", authToken);
+        for (String key : queries.keySet()) {
+            clientResource.setQueryValue(key, queries.get(key));
+        }
+        return clientResource.put(entity, outputClass);
+    }
+
 }
