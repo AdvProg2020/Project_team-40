6e4197354c57805c93dd6b688361470827da3938
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-07-10 18:46:49 +0430

    Small adjustments in OrdersItem

--- src/main/java/view/account_menus/customer_view/orders_view/OrderItem.java
+++ src/main/java/view/account_menus/customer_view/orders_view/OrderItem.java
@@ -1,7 +1,9 @@
 package view.account_menus.customer_view.orders_view;
 
 import com.jfoenix.controls.JFXButton;
+import controller.accounts.AccountController;
 import controller.accounts.CustomerAccountController;
+import controller.accounts.SellerAccountController;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -13,34 +15,13 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.log.Log;
+import model.users.Customer;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class OrderItem implements Initializable {
+public class OrderItem {
     public JFXButton viewLogButton;
-    private CustomerAccountController customerAccountController;
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        customerAccountController = CustomerAccountController.getInstance();
-
-    }
-
-    private void loadLogs(VBox vBoxItems) {
-        vBoxItems.getChildren().clear();
-        for (String logId : customerAccountController.getOrders().keySet()) {
-            try {
-                Log log = customerAccountController.getOrder(logId);
-                AnchorPane item = FXMLLoader.load(getClass().getResource("/layouts/customer_menus/customer_orders_menu/order_item.fxml"));
-                HBox hBox = (HBox) item.getChildren().get(0);
-                setLabelsContent(log, hBox);
-                vBoxItems.getChildren().add(item);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
 
     private void setLabelsContent(OrderMenu orderMenu, Log log) {
         Platform.runLater(new Runnable() {
@@ -57,25 +38,12 @@ public class OrderItem implements Initializable {
         });
     }
 
-    private void setLabelsContent(Log log, HBox hBox) {
-        Label logIdLabel = ((Label) hBox.getChildren().get(0));
-        Label dateLabel = ((Label) hBox.getChildren().get(1));
-        Label costLabel = ((Label) hBox.getChildren().get(2));
-        Label discountLabel = ((Label) hBox.getChildren().get(3));
-
-        logIdLabel.setText(log.getId());
-        dateLabel.setText(log.getDate().toString());
-        costLabel.setText(Double.toString(log.getCost()));
-        discountLabel.setText(Double.toString(log.getCostWithoutDiscount()));
-    }
-
-
     public void handleViewLog() {
         HBox item = (HBox)(viewLogButton.getParent());
         String logId = ((Label) item.getChildren().get(0)).getText();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/customer_menus/customer_orders_menus/order.fxml"));
         try {
-            Log log = customerAccountController.getOrder(logId);
+            Log log = Log.getLogById(logId);
             AnchorPane pane = loader.load();
             OrderMenu orderMenu = loader.getController();
             setLabelsContent(orderMenu, log);
