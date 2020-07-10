7c4f235e41429bb50177e4160cc927c489df9732
mtndaghyani <matin.daghyani@gmail.com>
2020-07-10 18:45:37 +0430

    Add a customized StatusService to handle exceptions thrown in the server

--- src/main/java/server/MainServer.java
+++ src/main/java/server/MainServer.java
@@ -1,7 +1,14 @@
 package server;
 
 import org.restlet.Component;
+import org.restlet.Request;
+import org.restlet.Response;
 import org.restlet.data.Protocol;
+import org.restlet.data.Status;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.resource.Resource;
+import org.restlet.service.StatusService;
 import server.server_resources.manager_account_controller.*;
 
 public class MainServer extends Component {
@@ -12,6 +19,17 @@ public class MainServer extends Component {
 
 
     public MainServer() {
+        setStatusService(new StatusService(){
+            @Override
+            public Representation getRepresentation(Status status, Request request, Response response) {
+                return new StringRepresentation(status.getThrowable().getMessage());
+            }
+
+            @Override
+            public Status getStatus(Throwable throwable, Resource resource) {
+                return Status.CLIENT_ERROR_FORBIDDEN;
+            }
+        });
         getServers().add(Protocol.HTTP, DEFAULT_PORT);
         getDefaultHost().attach("/accounts/manager_account_controller/manager/", ManagerResource.class);
         getDefaultHost().attach("/accounts/manager_account_controller/user/", UserResource.class);
