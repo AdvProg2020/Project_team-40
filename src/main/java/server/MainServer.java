e1b56d3765066617d8297b23a62d9d2f980d0165
mtndaghyani <matin.daghyani@gmail.com>
2020-07-10 19:46:49 +0430

    Add customer resources URIs to server

--- src/main/java/server/MainServer.java
+++ src/main/java/server/MainServer.java
@@ -9,6 +9,7 @@ import org.restlet.representation.Representation;
 import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.Resource;
 import org.restlet.service.StatusService;
+import server.server_resources.customer_account_controller.*;
 import server.server_resources.manager_account_controller.*;
 
 public class MainServer extends Component {
@@ -33,14 +34,28 @@ public class MainServer extends Component {
         getServers().add(Protocol.HTTP, DEFAULT_PORT);
         getDefaultHost().attach("/accounts/manager_account_controller/manager/", ManagerResource.class);
         getDefaultHost().attach("/accounts/manager_account_controller/user/", UserResource.class);
-        getDefaultHost().attach("/accounts/manager_account_controller/product/", ProductResource.class);
+        getDefaultHost().attach("/accounts/manager_account_controller/product/", ManagerProductResource.class);
         getDefaultHost().attach("/accounts/manager_account_controller/all_categories/", AllCategoriesResource.class);
         getDefaultHost().attach("/accounts/manager_account_controller/category/", CategoryResource.class);
         getDefaultHost().attach("/accounts/manager_account_controller/request/", RequestResource.class);
         getDefaultHost().attach("/accounts/manager_account_controller/accept_request/", AcceptRequestResource.class);
         getDefaultHost().attach("/accounts/manager_account_controller/decline_request/", DeclineRequestResource.class);
-        getDefaultHost().attach("/accounts/manager_account_controller/discount/", DiscountResource.class);
-        getDefaultHost().attach("/accounts/manager_account_controller/all_discounts/", AllDiscountsResource.class);
+        getDefaultHost().attach("/accounts/manager_account_controller/discount/", ManagerDiscountResource.class);
+        getDefaultHost().attach("/accounts/manager_account_controller/all_discounts/", ManagerAllDiscountsResource.class);
+
+        getDefaultHost().attach("/accounts/customer_account_controller/all_discounts/", CustomerAllDiscountsResource.class);
+        getDefaultHost().attach("/accounts/customer_account_controller/balance/", BalanceResource.class);
+        getDefaultHost().attach("/accounts/customer_account_controller/cart/", CartResource.class);
+        getDefaultHost().attach("/accounts/customer_account_controller/cart_total_price/", CartTotalPriceResource.class);
+        getDefaultHost().attach("/accounts/customer_account_controller/discount/", CustomerDiscountResource.class);
+        getDefaultHost().attach("/accounts/customer_account_controller/product/", CustomerProductResource.class);
+        getDefaultHost().attach("/accounts/customer_account_controller/customer/", CustomerResource.class);
+        getDefaultHost().attach("/accounts/customer_account_controller/order/", OrderResource.class);
+        getDefaultHost().attach("/accounts/customer_account_controller/orders/", OrdersResource.class);
+        getDefaultHost().attach("/accounts/customer_account_controller/payment/", PaymentResource.class);
+        getDefaultHost().attach("/accounts/customer_account_controller/price_after_discount/", PriceAfterDiscountResource.class);
+        getDefaultHost().attach("/accounts/customer_account_controller/price_without_discount/", PriceWithoutDiscountResource.class);
+        getDefaultHost().attach("/accounts/customer_account_controller/receiver_info/", ReceiverInfoResource.class);
 
 
 
