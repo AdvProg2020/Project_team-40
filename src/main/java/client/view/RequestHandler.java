df2ed829d548d6404cb34c1a9fb7ad8a00e497df
mtndaghyani <matin.daghyani@gmail.com>
2020-07-11 21:53:57 +0430

    Add AccountResource to server resources

--- src/main/java/client/view/RequestHandler.java
+++ src/main/java/client/view/RequestHandler.java
@@ -1,13 +1,19 @@
 package client.view;
 
 import org.restlet.resource.ClientResource;
+import server.model.users.User;
 
 import java.util.HashMap;
 
 public class RequestHandler {
     private static String authToken;
     private static boolean isLoggedIn;
+    private static User thisUser;
     private static final String ENDPOINT = "http://localhost:8080";
+
+    public static User getThisUser() {
+        return thisUser;
+    }
     //TODO: Configure the endpoint from vmf file
 
     public static void setAuthToken(String token){
@@ -16,6 +22,10 @@ public class RequestHandler {
 
     public static void setIsLoggedIn(boolean isLoggedIn) {
         RequestHandler.isLoggedIn = isLoggedIn;
+    }
+
+    public static boolean isIsLoggedIn() {
+        return isLoggedIn;
     }
 
     public static <T> Object get(String path, HashMap<String, String> queries, boolean doesSendToken, Class<T> outputClass) {
--- src/main/java/server/server_resources/accounts/AccountResource.java
+++ src/main/java/server/server_resources/accounts/AccountResource.java
@@ -1,17 +1,22 @@
 package server.server_resources.accounts;
 
 import exceptions.AccountsException;
-import org.restlet.resource.Put;
+import org.restlet.resource.Delete;
+import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
-import server.AuthenticationTokenHandler;
 import server.controller.accounts.AccountController;
 
 public class AccountResource extends ServerResource {
 
-    @Put
+    @Post
     public String login(String username) throws AccountsException {
         String password = getQueryValue("password");
         return AccountController.getInstance().login(username, password);
+    }
+
+    @Delete
+    public void logout(String token){
+        AccountController.getInstance().logout();
     }
 
 
