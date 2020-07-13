362ff91dc1bf099d3086f53b11b44831ef7628c0
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-07-13 16:36:07 +0430

    Partly designed create_receipt.fxml

--- src/main/java/client/view/bank/receipts/CreateReceiptManager.java
+++ src/main/java/client/view/bank/receipts/CreateReceiptManager.java
@@ -1,4 +1,14 @@
 package client.view.bank.receipts;
 
+import javafx.scene.input.MouseEvent;
+
 public class CreateReceiptManager {
+    public void deposit() {
+    }
+
+    public void withdraw() {
+    }
+
+    public void move() {
+    }
 }
