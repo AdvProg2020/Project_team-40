04f713087643df5fe8f151ed27ed0bba68473328
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-07-14 10:11:33 +0430

    Created manage_credit_menu.fxml and ManageCreditMenuManager and completed manage_credit_menu.fxml

---
+++ src/main/java/client/view/account_menus/ManageCreditMenuManager.java
@@ -0,0 +1,10 @@
+package client.view.account_menus;
+
+import client.view.MenuManager;
+import javafx.scene.input.MouseEvent;
+
+public class ManageCreditMenuManager extends MenuManager {
+
+    public void moveCredit() {
+    }
+}
--- src/main/java/client/view/account_menus/WalletManager.java
+++ src/main/java/client/view/account_menus/WalletManager.java
@@ -3,4 +3,9 @@ package client.view.account_menus;
 import client.view.MenuManager;
 
 public class WalletManager extends MenuManager {
+    public void moveCreditToBank() {
+    }
+
+    public void moveCreditToWallet() {
+    }
 }
