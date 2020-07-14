fe0eed70938a52fc2b421f0c9e7ab5ff59a7dc91
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-07-14 12:22:27 +0430

    Designed auction_chat_room.fxml

---
+++ src/main/java/client/view/account_menus/customer_view/AuctionChatManager.java
@@ -0,0 +1,10 @@
+package client.view.account_menus.customer_view;
+
+import client.view.MenuManager;
+import javafx.event.ActionEvent;
+
+public class AuctionChatManager extends MenuManager {
+
+    public void sendMessage() {
+    }
+}
