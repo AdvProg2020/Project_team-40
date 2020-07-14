fe5cc87bb143d6aa0ed538ee12930724d656831e
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-07-14 14:49:30 +0430

    Partly implemented auction.fxml

---
+++ src/main/java/client/view/account_menus/customer_seller_common_view/AuctionManager.java
@@ -0,0 +1,4 @@
+package client.view.account_menus.customer_seller_common_view;
+
+public class AuctionManager {
+}
--- src/main/java/client/view/account_menus/customer_view/AuctionChatManager.java
+++ src/main/java/client/view/account_menus/customer_view/AuctionChatManager.java
@@ -1,10 +1,12 @@
 package client.view.account_menus.customer_view;
 
 import client.view.MenuManager;
-import javafx.event.ActionEvent;
 
 public class AuctionChatManager extends MenuManager {
 
     public void sendMessage() {
+    }
+
+    public void refresh() {
     }
 }
