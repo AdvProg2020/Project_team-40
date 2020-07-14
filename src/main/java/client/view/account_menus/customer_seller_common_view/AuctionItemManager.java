65d83c684035c914af3deda45f5a5e70b8c5b072
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-07-14 11:32:00 +0430

    Created auction_item.fxml and AuctionItemManager

---
+++ src/main/java/client/view/account_menus/customer_seller_common_view/AuctionItemManager.java
@@ -0,0 +1,10 @@
+package client.view.account_menus.customer_seller_common_view;
+
+public class AuctionItemManager {
+
+    public void viewAuctionInfo() {
+    }
+
+    public void goToChatRoom() {
+    }
+}
