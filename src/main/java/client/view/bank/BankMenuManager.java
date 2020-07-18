bc2084e0a378d832ca05eb4321972f9e4ff2976f
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-07-18 18:20:56 +0430

    Created default_bank_menu.fxml and DefaultMenuManager

--- src/main/java/client/view/bank/BankMenuManager.java
+++ src/main/java/client/view/bank/BankMenuManager.java
@@ -1,15 +1,27 @@
 package client.view.bank;
 
-import client.view.MenuManager;
-import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class BankMenuManager extends MenuManager {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BankMenuManager implements Initializable {
+    public Pane innerPane;
     public Button exitButton;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setInnerPane("/layouts/bank_menus/default_bank_menu.fxml");
+    }
+
     public void goToHome() {
+
     }
 
     public void goToReceipts() {
@@ -19,9 +31,21 @@ public class BankMenuManager extends MenuManager {
     }
 
     public void viewBalance() {
+
     }
 
     public void exitBankAccount() {
         ((Stage)(exitButton.getScene().getWindow())).close();
+    }
+
+    private void setInnerPane(String rootLocation) {
+        innerPane.getChildren().clear();
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource(rootLocation));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        innerPane.getChildren().add(root);
     }
 }
---
+++ src/main/java/client/view/bank/DefaultMenuManager.java
@@ -0,0 +1,4 @@
+package client.view.bank;
+
+public class DefaultMenuManager {
+}
