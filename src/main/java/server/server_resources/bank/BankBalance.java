5f2fbdf7f0e85deb82512694b92135b88a093383
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-07-18 19:24:43 +0430

    Implemented BankBalance

--- src/main/java/server/server_resources/bank/BankBalance.java
+++ src/main/java/server/server_resources/bank/BankBalance.java
@@ -1,7 +1,31 @@
 package server.server_resources.bank;
 
+import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
+import server.controller.menus.BankController;
+
+import java.io.*;
+import java.net.Socket;
+
+import static server.server_resources.bank.BankInformation.BANK_PORT;
+import static server.server_resources.bank.BankInformation.IP;
 
 public class BankBalance extends ServerResource {
+    @Get
+    public String getBalance() {
+        String token = BankController.getBankController().getUsersTokens().get(getQueryValue("username"));
+        try {
+            Socket socket = new Socket(IP, BANK_PORT);
+            DataOutputStream outputStream = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+            outputStream.writeUTF("get_balance " + token);
+            outputStream.flush();
+            DataInputStream inputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            String bankResponse = inputStream.readUTF();
+            socket.close();
+            return bankResponse;
+        } catch (IOException e) {
+            return e.getMessage();
+        }
 
+    }
 }
