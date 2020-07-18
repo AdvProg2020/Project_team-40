a6a1341094a8830f6f107b4248c62761e4027838
mtndaghyani <matin.daghyani@gmail.com>
2020-07-18 16:46:47 +0430

    Get list of Category objects instead of list of their names

--- src/main/java/server/server_resources/shop/AllCategoriesResource.java
+++ src/main/java/server/server_resources/shop/AllCategoriesResource.java
@@ -5,13 +5,14 @@ import com.gilecode.yagson.com.google.gson.reflect.TypeToken;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 import server.controller.menus.AllProductsController;
+import server.model.Category;
 
 import java.util.ArrayList;
 
 public class AllCategoriesResource extends ServerResource {
     @Get
     public String getAllCategories (){
-        ArrayList<String> allCats = AllProductsController.getInstance().getAllCategories();
-        return new YaGson().toJson(allCats, new TypeToken<ArrayList<String>>(){}.getType());
+        ArrayList<Category> allCats = AllProductsController.getInstance().getAllCategories();
+        return new YaGson().toJson(allCats, new TypeToken<ArrayList<Category>>(){}.getType());
     }
 }
