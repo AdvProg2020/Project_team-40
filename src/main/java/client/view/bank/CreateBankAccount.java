bd560bb899ce89d6ba8ccd4f3386f18b82d54c87
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-07-18 14:42:57 +0430

    Small adjustments

--- src/main/java/client/view/bank/CreateBankAccount.java
+++ src/main/java/client/view/bank/CreateBankAccount.java
@@ -3,19 +3,13 @@ package client.view.bank;
 import client.controller.Client;
 import client.controller.RequestHandler;
 import client.view.MenuManager;
-import client.view.ValidInput;
+import com.gilecode.yagson.com.google.gson.reflect.TypeToken;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.net.Socket;
 import java.util.HashMap;
-import java.util.Queue;
 
 public class CreateBankAccount extends MenuManager {
     public Label errorMessage;
@@ -30,6 +24,7 @@ public class CreateBankAccount extends MenuManager {
             errorMessage.setText("Fill all the above Fields!");
             return;
         }
+        System.out.println("hi");
         HashMap<String, String> queries = new HashMap<>();
         Client client = Client.getInstance();
         queries.put("first name", client.getFirstName());
@@ -39,27 +34,10 @@ public class CreateBankAccount extends MenuManager {
         queries.put("repeat password", repeatPasswordField.getText());
         queries.put("username", client.getUsername());
         String response = RequestHandler.get("/bank/register/", queries, true, String.class);
-        System.out.println(response);
-        successfullyLogin(response);
+        goToAccount(response);
     }
-    /*
-    try {
-                Socket socket = new Socket(IP, BANK_PORT);
-                DataOutputStream outputStream = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));;
-                outputStream.writeUTF("create_account " + Client.getInstance().getFirstName() + " " +
-                Client.getInstance().getLastName() + " " + usernameField.getText() + " " + passwordField.getText() + " "
-                + repeatPasswordField.getText());
-                outputStream.flush();
-                DataInputStream inputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-                handleResponse(inputStream.readUTF(), Client.getInstance().getUsername());
-                socket.close();
-            } catch (Exception e) {
-                errorMessage.setText(e.getMessage());
-            }
 
-     */
-
-    private void successfullyLogin(String response) {
+    private void goToAccount(String response) {
         try {
             int accountNumber = Integer.parseInt(response);
             usernameField.setDisable(true);
