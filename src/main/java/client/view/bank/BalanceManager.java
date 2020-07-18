1d276841a52c627c65930fa839c0636ed3262af2
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-07-18 19:14:25 +0430

    Created a ServerResource class to handle getting balance

--- src/main/java/client/view/bank/BalanceManager.java
+++ src/main/java/client/view/bank/BalanceManager.java
@@ -1,17 +1,24 @@
 package client.view.bank;
 
+import client.controller.Client;
+import client.controller.RequestHandler;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 public class BalanceManager implements Initializable {
     public Label balanceLabel;
     public Label titleLabel;
+    //TODO: What I wanted to do with the titleLabel????
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        //TODO:
+        HashMap<String, String> queries = new HashMap<>();
+        queries.put("username", Client.getInstance().getUsername());
+        String response = RequestHandler.get("/bank/balance/", queries, true, String.class);
+
     }
 }
---
+++ src/main/java/server/server_resources/bank/BankBalance.java
@@ -0,0 +1,7 @@
+package server.server_resources.bank;
+
+import org.restlet.resource.ServerResource;
+
+public class BankBalance extends ServerResource {
+
+}
