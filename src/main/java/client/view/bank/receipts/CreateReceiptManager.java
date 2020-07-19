af56ed124d4c67101b988fb10b571fdda6319afc
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-07-19 17:35:35 +0430

    Partly implemented CreateReceiptManager

--- src/main/java/client/view/bank/receipts/CreateReceiptManager.java
+++ src/main/java/client/view/bank/receipts/CreateReceiptManager.java
@@ -1,11 +1,14 @@
 package client.view.bank.receipts;
 
+import client.view.ValidInput;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 public class CreateReceiptManager implements Initializable {
@@ -14,7 +17,15 @@ public class CreateReceiptManager implements Initializable {
     public RadioButton move;
     public TextField sourceIDField;
     public TextField destinationIDField;
+    public TextField descriptionField;
     public TextField moneyField;
+
+    private final String BLANK_ERROR = "Fill this field!";
+    public Label toggleGroupError;
+    public Label sourceIDError;
+    public Label destinationIDError;
+    public Label amountError;
+    public Label serverError;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -40,6 +51,37 @@ public class CreateReceiptManager implements Initializable {
     }
 
     public void createReceipt() {
-        //TODO:
+        try {
+            validateFieldsInput();
+            HashMap<String, String> queries = new HashMap<>();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void validateFieldsInput() throws Exception {
+        if(moneyField.getText().isBlank()) {
+            amountError.setText(BLANK_ERROR);
+            throw new Exception();
+        }
+        if(!deposit.isSelected() && !withdraw.isSelected() && !move.isSelected()) {
+            toggleGroupError.setText("Choose a type!");
+            throw new Exception();
+        }
+        if(!ValidInput.INTEGER.getStringMatcher(moneyField.getText()).matches()) {
+            amountError.setText("Enter a number!");
+            throw new Exception();
+        }
+        validateFieldsInput(sourceIDField, sourceIDError);
+        validateFieldsInput(destinationIDField, destinationIDError);
+
+    }
+
+    private void validateFieldsInput(TextField idField, Label idError) throws Exception {
+        if(!idField.isDisable() && (idField.getText().isBlank() ||
+                !ValidInput.INTEGER.getStringMatcher(idField.getText()).matches())) {
+            idError.setText("Enter a number!");
+            throw new Exception();
+        }
     }
 }
