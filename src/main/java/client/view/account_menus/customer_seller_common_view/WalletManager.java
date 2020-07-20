0b072168107f0a6a6de99ef1c1a9ee4034bd5b5e
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-07-20 12:19:14 +0430

    Created class WalletResource

--- src/main/java/client/view/account_menus/customer_seller_common_view/WalletManager.java
+++ src/main/java/client/view/account_menus/customer_seller_common_view/WalletManager.java
@@ -1,9 +1,22 @@
 package client.view.account_menus.customer_seller_common_view;
 
 import client.view.MenuManager;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
-public class WalletManager extends MenuManager {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WalletManager extends MenuManager implements Initializable {
+    public Label walletCredit;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
     public void moveCreditToBank() {
+
     }
 
     public void moveCreditToWallet() {
---
+++ src/main/java/server/server_resources/seller_customer_common/WalletResource.java
@@ -0,0 +1,11 @@
+package server.server_resources.seller_customer_common;
+
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+
+public class WalletResource extends ServerResource {
+    @Get
+    public String getWalletCredit() {
+        return null;
+    }
+}
