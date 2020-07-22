6bafbc265d3a8e7f4f530ec662cd89446a92b4ab
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-07-22 16:56:50 +0430

    Implemented TransactionsMenuManager

--- src/main/java/client/view/bank/transactions/TransactionsMenuManager.java
+++ src/main/java/client/view/bank/transactions/TransactionsMenuManager.java
@@ -4,7 +4,13 @@ import client.controller.Client;
 import client.controller.RequestHandler;
 import client.view.ValidInput;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.ResourceBundle;
@@ -23,26 +29,28 @@ public class TransactionsMenuManager implements Initializable {
     }
 
     private void showTransactions(String transaction) {
+        //TODO: the line below: Caused by: java.lang.StringIndexOutOfBoundsException: begin 1, end -2, length 0
         transaction = transaction.substring(1, transaction.length() - 2);
         System.out.println(transaction);
         String[] information = transaction.split(",");
-        for(int i = 0; i < 6; i++) {
-            setInformationInLabel(i, information[i]);
+        AnchorPane pane = null;
+        try {
+            pane = FXMLLoader.load(getClass().getResource("/layouts/bank_menus/receipts/receipt_item.fxml"));
+            HBox item = (HBox) pane.getChildren().get(0);
+            for(int i = 0; i < 6; i++) {
+                setInformationInLabel(5 - i, information[i], item);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
     }
 
-    private void setInformationInLabel(int index, String information) {
+    private void setInformationInLabel(int index, String information, HBox item) {
         String data = information.split(":")[1];
-        if(ValidInput.INTEGER.getStringMatcher(data).matches()) {
-            setItem(data);
-        } else {
-            setItem(data.substring(1, data.length() - 2));
+        if(!ValidInput.INTEGER.getStringMatcher(data).matches()) {
+            data = data.substring(1, data.length() - 2);
         }
-    }
-
-    private void setItem(String data) {
-
+        ((Label) item.getChildren().get(index)).setText(information);
     }
 
     public void createReceipt() {
