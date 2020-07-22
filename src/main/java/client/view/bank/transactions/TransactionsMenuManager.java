eadce4407980aeb2f4c70a2f21a774a7f10bb779
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-07-22 16:39:08 +0430

    Partly implemented TransactionsMenuManager

--- src/main/java/client/view/bank/transactions/TransactionsMenuManager.java
+++ src/main/java/client/view/bank/transactions/TransactionsMenuManager.java
@@ -2,6 +2,7 @@ package client.view.bank.transactions;
 
 import client.controller.Client;
 import client.controller.RequestHandler;
+import client.view.ValidInput;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import java.net.URL;
@@ -14,6 +15,34 @@ public class TransactionsMenuManager implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         String response = RequestHandler.put("/bank/transactions/", Client.getInstance().getUsername(),
                 new HashMap<>(), true, String.class);
+        System.out.println(response);
+        String[] transactions = response.split("/*");
+        for(String transaction : transactions) {
+            showTransactions(transaction);
+        }
+    }
+
+    private void showTransactions(String transaction) {
+        transaction = transaction.substring(1, transaction.length() - 2);
+        System.out.println(transaction);
+        String[] information = transaction.split(",");
+        for(int i = 0; i < 6; i++) {
+            setInformationInLabel(i, information[i]);
+        }
+
+    }
+
+    private void setInformationInLabel(int index, String information) {
+        String data = information.split(":")[1];
+        if(ValidInput.INTEGER.getStringMatcher(data).matches()) {
+            setItem(data);
+        } else {
+            setItem(data.substring(1, data.length() - 2));
+        }
+    }
+
+    private void setItem(String data) {
+
     }
 
     public void createReceipt() {
--- src/main/java/client/view/bank/transactions/transactionManager.java
+++
@@ -1,4 +0,0 @@
-package client.view.bank.transactions;
-
-public class transactionManager {
-}
