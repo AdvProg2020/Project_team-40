cbfec08030a75f83a521dfa35f57b7a940fc4f90
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-07-23 11:27:24 +0430

    Implemented AuctionItem for Seller

--- src/main/java/client/view/account_menus/seller_view/auction_view/AuctionItem.java
+++ src/main/java/client/view/account_menus/seller_view/auction_view/AuctionItem.java
@@ -1,8 +1,25 @@
 package client.view.account_menus.seller_view.auction_view;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AuctionItem {
-    public void viewProduct() {
+    public void viewAuction() {
+        Stage stage = new Stage();
+        try {
+            Parent root = FXMLLoader.load(getClass().
+                    getResource("/layouts/customer_seller_common_menus/auction_menus/auction.fxml"));
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(new Scene(root, 800, 600));
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
