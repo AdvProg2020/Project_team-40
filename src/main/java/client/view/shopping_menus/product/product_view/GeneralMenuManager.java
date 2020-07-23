1eb8c3bb9654f93b235f56d931a102c521c170b9
soligits <m.soltani.252@gmail.com>
2020-07-23 08:42:37 +0430

    Finished file.

--- src/main/java/client/view/shopping_menus/product/product_view/GeneralMenuManager.java
+++ src/main/java/client/view/shopping_menus/product/product_view/GeneralMenuManager.java
@@ -3,26 +3,33 @@ package client.view.shopping_menus.product.product_view;
 import client.controller.Client;
 import client.controller.RequestHandler;
 import client.view.MenuManager;
+import com.gilecode.yagson.com.google.gson.reflect.TypeToken;
 import com.jfoenix.controls.JFXButton;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import javafx.util.Pair;
 import org.restlet.resource.ResourceException;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.ResourceBundle;
 
 public class GeneralMenuManager extends MenuManager implements Initializable{
+    private static final String DESTINATION = "C:\\App-resource\\";
 
+    public AnchorPane root;
     public Text productName, sellerName, companyName, category, rating, descriptionText, priceText;
     public Spinner<Integer> countSpinner;
     public ImageView imageView;
-    public JFXButton cartButton;
+    public JFXButton cartButton, downloadButton;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle){
@@ -34,6 +41,23 @@ public class GeneralMenuManager extends MenuManager implements Initializable{
         rating.setText(ProductMenuManager.getProduct().getAverageScore() + " / " + "5.0");
         descriptionText.setText(ProductMenuManager.getProduct().getExplanation());
         priceText.setText(ProductMenuManager.getProduct().getPrice() + " / " + ProductMenuManager.getProduct().getBasePrice());
+
+        if(!ProductMenuManager.getProduct().hasFile())
+            root.getChildren().remove(downloadButton);
+
+        HashMap<String, String> requestQueries = new HashMap<>();
+        requestQueries.put("username", Client.getInstance().getUsername());
+        HashMap<String, String> userParams = RequestHandler.get("/accounts/user/", requestQueries,
+                false, new TypeToken<HashMap<String, String>>(){}.getType());
+        assert userParams != null;
+
+        requestQueries.clear();
+        requestQueries.put("username", userParams.get("username"));
+        requestQueries.put("productID", ProductMenuManager.getProduct().getProductId());
+        boolean hasBought = (boolean) RequestHandler.get("/accounts/customer_account_controller/product/purchase_status/", requestQueries, false, boolean.class);
+        if(!hasBought)
+            downloadButton.setDisable(true);
+
         if (!Client.getInstance().isLoggedIn() || Client.getInstance().getRole().equals("Customer")) {
             //Work the functionality of product count
             SpinnerValueFactory<Integer> spinnerValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, ProductMenuManager.getProduct().getCount(), 0);
@@ -64,6 +88,22 @@ public class GeneralMenuManager extends MenuManager implements Initializable{
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
+        }
+    }
+
+    public void download(){
+        HashMap<String, String> requestQueries = new HashMap<>();
+        requestQueries.put("productId", ProductMenuManager.getProduct().getProductId());
+        Pair<String, byte[]> pair = RequestHandler.get("/shop/file/", requestQueries, true, new TypeToken<Pair<String, byte[]>>(){}.getType());
+        File file = new File(DESTINATION + pair.getKey());
+        try {
+            if (!file.exists())
+                if(!file.createNewFile())
+                    throw new IOException("File creation failed.");
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(pair.getValue());
+        }catch(IOException e){
+            System.out.println(e.getMessage());
         }
     }
 
