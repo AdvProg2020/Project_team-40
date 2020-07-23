d4a339fcc648d27b3ccd867ac95a4b64becaac6a
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-07-23 11:16:08 +0430

    Created product_item.fxml to show products while creating auctions

--- src/main/java/client/view/account_menus/seller_view/accounts_view/AddAuctionManager.java
+++ src/main/java/client/view/account_menus/seller_view/accounts_view/AddAuctionManager.java
@@ -1,18 +1,23 @@
-package client.view.account_menus.seller_view;
+package client.view.account_menus.seller_view.accounts_view;
 
 import client.controller.Client;
 import client.controller.RequestHandler;
 import com.gilecode.yagson.com.google.gson.reflect.TypeToken;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import server.model.Product;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.ResourceBundle;
 
 public class AddAuctionManager implements Initializable {
+    public VBox vBoxItems;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -26,7 +31,12 @@ public class AddAuctionManager implements Initializable {
     }
 
     private void setProductsItem(Product product) {
-
+        try {
+            AnchorPane item = FXMLLoader.load(getClass().
+                    getResource("/layouts/seller_menus/sellers_auctions_menus/product_item.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void addAuction() {
