5ccdf18cf43a15a7a989394b64f1891d438fae1f
mtndaghyani <matin.daghyani@gmail.com>
2020-07-23 19:59:18 +0430

    Set wage percentage and minimum wallet credit in the financial menu

--- src/main/java/client/view/account_menus/manager_view/FinancialResourcesManager.java
+++ src/main/java/client/view/account_menus/manager_view/FinancialResourcesManager.java
@@ -2,12 +2,16 @@ package client.view.account_menus.manager_view;
 
 import client.controller.RequestHandler;
 import com.gilecode.yagson.com.google.gson.reflect.TypeToken;
+import com.jfoenix.controls.JFXTextField;
+import exceptions.InvalidInputException;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import org.restlet.resource.ResourceException;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.ResourceBundle;
@@ -15,17 +19,15 @@ import java.util.ResourceBundle;
 public class FinancialResourcesManager implements Initializable {
     public Label usernameLabel;
     public Label accountIDLabel;
-    public Label minCreditLabel;
-    public Label wageLabel;
-
-    public Label minCreditError;
-    public Label wageError;
+    public Label errorLabel;
 
     public Button minCreditButton;
     public Button wageButton;
+    public Button saveMinCredit;
+    public Button saveWage;
+    public JFXTextField minCreditTextField;
+    public JFXTextField wageTextField;
 
-    private TextField minCreditField;
-    private TextField wageField;
 
     public GridPane gridPane;
 
@@ -36,7 +38,63 @@ public class FinancialResourcesManager implements Initializable {
                 new HashMap<>(), true, new TypeToken<HashMap<String, String>>(){}.getType());
         usernameLabel.setText(information.get("bank username"));
         accountIDLabel.setText(information.get("bank id"));
-        wageLabel.setText(information.get("wage"));
-        minCreditLabel.setText(information.get("min balance"));
+    }
+
+
+    public void handleReadMinCredit(ActionEvent event) {
+        minCreditTextField.setDisable(false);
+        saveMinCredit.setDisable(false);
+    }
+
+    public void handleReadWage(ActionEvent event) {
+        wageTextField.setDisable(false);
+        saveWage.setDisable(false);
+    }
+
+    public void handleSaveMinCredit(ActionEvent event) {
+        String credit = minCreditTextField.getText();
+        try {
+            double amount = Double.parseDouble(credit);
+            if (amount < 0)
+                throw new InvalidInputException("The amount is less than 0!");
+            RequestHandler.put("/accounts/manager_account_controller/min_wallet_credit/", credit, new HashMap<>(), true, null);
+            minCreditTextField.setDisable(true);
+            saveMinCredit.setDisable(true);
+        } catch (NumberFormatException e) {
+            errorLabel.setText("Wrong input format");
+        } catch (InvalidInputException e) {
+            errorLabel.setText(e.getMessage());
+        }
+        catch (ResourceException e){
+            try {
+                errorLabel.setText(RequestHandler.getClientResource().getResponseEntity().getText());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public void handleSaveWage(ActionEvent event) {
+        String wagePercentage = wageTextField.getText();
+        try {
+            int wage = Integer.parseInt(wagePercentage);
+            if (wage < 0 || wage > 100)
+                throw new InvalidInputException("Enter a number between 0 and 100");
+            RequestHandler.put("/accounts/manager_account_controller/wage/", wagePercentage, new HashMap<>(), true, null);
+            wageTextField.setDisable(true);
+            saveWage.setDisable(true);
+        }catch (NumberFormatException e){
+            errorLabel.setText("Wrong input format");
+        } catch (InvalidInputException e) {
+            errorLabel.setText(e.getMessage());
+        }
+        catch (ResourceException e){
+            try {
+                errorLabel.setText(RequestHandler.getClientResource().getResponseEntity().getText());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
     }
 }
