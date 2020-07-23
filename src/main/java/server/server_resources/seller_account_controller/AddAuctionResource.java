f96bbd3c74d3abbba1b024a04afdd4191c51457a
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-07-23 18:45:56 +0430

    Fixed a bug

--- src/main/java/server/server_resources/seller_account_controller/AddAuctionResource.java
+++ src/main/java/server/server_resources/seller_account_controller/AddAuctionResource.java
@@ -1,5 +1,6 @@
 package server.server_resources.seller_account_controller;
 
+import com.gilecode.yagson.YaGson;
 import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
 import server.controller.menus.ChatController;
@@ -11,7 +12,8 @@ import java.util.Date;
 
 public class AddAuctionResource extends ServerResource {
     @Post
-    public void addAuction(Date deadline) {
+    public void addAuction(String entity) {
+        Date deadline = new YaGson().fromJson(entity, Date.class);
         String productId = getQueryValue("product ID");
         new Auction(deadline, productId, (int) Product.getProductById(productId).getPrice(), getQueryValue("seller ID"),
                 ChatController.getInstance().createChat("Auction for " + Product.getProductById(productId).getName()).getId());
