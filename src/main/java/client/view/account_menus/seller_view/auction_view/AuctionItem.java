d595c8f804088284da82e4759fbd87fdf7aae816
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-07-24 09:34:13 +0430

    Small adjustments in AuctionItem and AuctionItemManager

--- src/main/java/client/view/account_menus/seller_view/auction_view/AuctionItem.java
+++ src/main/java/client/view/account_menus/seller_view/auction_view/AuctionItem.java
@@ -18,7 +18,7 @@ public class AuctionItem {
         Stage stage = new Stage();
         try {
             Parent root = FXMLLoader.load(getClass().
-                    getResource("/layouts/customer_seller_common_menus/auction_menus/auction.fxml"));
+                    getResource("/layouts/customer_seller_common_menus/auctions_menus/auction.fxml"));
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.setScene(new Scene(root, 800, 600));
             stage.showAndWait();
